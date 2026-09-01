dba0609c5df2c8af270dd12702cbaadfc3205570
nffdev <nightfrost2discord@outlook.fr>
2026-09-01 21:08:23 +0200

    fix(back): added rate limit on write endpoints

--- Back/routes/comments.js
+++ Back/routes/comments.js
@@ -3,10 +3,11 @@ const router = Router();
 
 const { listByVideo, create, counts, remove } = require('../controllers/comments');
 const authMiddleware = require('../middleware/auth');
+const { writeLimiter } = require('../middleware/rateLimit');
 
 router.get('/counts', counts);
 router.get('/:videoId', listByVideo);
-router.post('/:videoId', authMiddleware, create);
-router.delete('/:commentId', authMiddleware, remove);
+router.post('/:videoId', authMiddleware, writeLimiter, create);
+router.delete('/:commentId', authMiddleware, writeLimiter, remove);
 
 module.exports = router;
--- Back/routes/likes.js
+++ Back/routes/likes.js
@@ -3,8 +3,9 @@ const router = Router();
 
 const { toggle, listMe, listUser } = require('../controllers/likes');
 const authMiddleware = require('../middleware/auth');
+const { writeLimiter } = require('../middleware/rateLimit');
 
-router.post('/:videoId', authMiddleware, toggle);
+router.post('/:videoId', authMiddleware, writeLimiter, toggle);
 router.get('/me', authMiddleware, listMe);
 router.get('/user/:id', listUser);
 
